3c9f665799be31930db4c6c9ecd4ff1a7fd57e90
S Sumesh <sumesh2003nov5@gmail.com>
2026-08-12 13:07:33 +0530

    Add: duplicate-integer - submission-80

---
+++ Data Structures & Algorithms/duplicate-integer/submission-80.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+         const seen = new Set<number>();
+
+        for (const num of nums) {
+            if (seen.has(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+
+        return false;
+    }
+}
